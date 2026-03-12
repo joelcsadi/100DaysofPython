2f37c37939991153c89d6db6cb822d5feb8257f2
JoCsadi <153603873+joelcsadi@users.noreply.github.com>
2026-03-12 18:55:39 +0000

    Add comments for Day 2 project

--- Day2-Project/expense-sharing-calculator.py
+++ Day2-Project/expense-sharing-calculator.py
@@ -1,3 +1,12 @@
+"""
+This is an expense sharing calculator that prompts the user
+for the total bill, the percent tip they want to add on, and
+how many people want to split the bill.
+
+It returns the price of how much each person has to pay
+if they all paid an equal percentage.
+"""
+
 print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill? $"))
 tip = float(input("What percentage tip would you like to give? 10 12 15 "))
